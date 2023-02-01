643c399892d8b5fca1cfafd4fb2944ed75c9b471
Immow <k.schippers@gmail.com>
2023-02-01 19:36:18 +0100

    bla

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,13 +1,8 @@
--- data.raw.recipe["se-core-fragment-uranium-ore"].results = {
--- 	{type = "item", name = "se-core-fragment-omni", amount_min = 0, amount_max = 4},
--- 	{"uranium-ore", 15},
--- 	{"stone", 1}
--- }
-data.raw.recipe["se-core-fragment-uranium-ore"].results = {
-	{type = "item", name = "se-core-fragment-omni", amount_min = 0, amount_max = 4},
-	{"uranium-ore", settings.startup["immow-uranium-ore"].value},
-	{"stone", 1}
-}
+for _, recipe in pairs(data.raw.recipe["se-core-fragment-uranium-ore"].results) do
+	if recipe.name == "uranium-ore" then
+		recipe.amount = settings.startup["immow-uranium-ore"].value
+	end
+end
 
 for _, recipe in pairs(data.raw.recipe["se-core-fragment-omni"].results) do
 	if recipe.name == "se-pyroflux" then
